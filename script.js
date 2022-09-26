3383e465f5fccd2b8e2730471d2a5561c1103296
rajeshpotter <er.rajesh.prajapati@gmail.com>
2022-09-26 23:38:04 +0530

    third commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Coder", "Developer", "Data Engineer", "Motivator"],
+        strings: ["Coder", "Software Developer", "Data Engineer", "Motivator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
